ba94cd2e7853d3e52de4d3cb7a12f0ba05670a67
caotavio <caotavio@hotmail.com>
2020-06-16 21:23:12 +0100

    Added text-based structures

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Footer from './components/footer'
 
 function App() {
   const Background = '../images/i-hungry-3.png';
+  const ProfilePic = '../images/profile.jpg';
 
   return (
     <>
@@ -24,10 +25,54 @@ function App() {
           </div>
           <h2 className="text-lg text-justify leading-none mt-5 lg:w-2/3">
             I am a Full Stack Web Developer from Brazil currently based in Ireland. I have a different story (to say the least), but <span
-            className="text-red-700">IF</span> you want to cut to the chase just scroll down and take a look at some of my projects... <span
-            className="text-red-700">ELSE</span>, <a href="#">click here</a> and get to know a bit more about me.
+            className="text-red-700">IF</span> you want to cut to the chase just <a href="#">click here</a> and take a look at some of my projects... <span
+            className="text-red-700">ELSE</span>, scroll down and get to know a bit more about me.
           </h2>
           <button className="mt-5">GET IN TOUCH</button>
+        </div>
+      </section>
+
+      <section> {/* ABOUT ME */}
+        <div className="py-10 bg-blue-300 lg:px-64 xl:px-1/5">
+          <h2 className="bg-blue-600">About Me</h2>
+          <div className="flex bg-blue-700">
+            <div>
+              <p>Greetings! I'm Otávio (Tav for some), a developer based in Ireland.</p>
+              <p>
+                I am a problem solver and self-motivated Web Developer skilled in building full Web Applications, that means I can work on both
+                back and frontend.
+              </p>
+              <p>
+                I've changed career paths 3 years ago from a busy Dental Surgeon and Orthodontist in Brazil to an avid coder and programming enthusiast.
+                I started learning code as a challenge because I love learning new abilites. Well, sufice to say that it turned into a hobby, then it
+                became a passion (as in I could not do anything else with my free time other than get in front of my laptop and build applications), so
+                I've decided to start a more structured course in Web Development with JavaScript, Node.JS, HTML and CSS.
+              </p>
+              <p>
+                A year later this passion took over my life and I had to make a big move... I quit in my job in Brazil and moved to Ireland to sharpen
+                my English skills and also to be able to dedicate myself fully to coding and making a career out of it.
+                I'm 31 now and it has been a beautiful journey so far. Since I moved here I was able to reach C1 (advanced) level in English and secured
+                a job as as a Full Stack Web Developer for a Digital Marketing company here in Ireland.
+              </p>
+              <p>
+                With a lot of hard work I am now capable of creating web apps and websites using a few other languages other than JavaScript (they are
+                listed below). My main focus now is to keep coding and honing my skills not only at the company I work in, but with personal projects.
+              </p>
+            </div>
+            <div className="relative pb-3/4 sm:w-full sm:pb-1/2 lg:pb-1/3">
+                <img src={ProfilePic} className="absolute w-full h-full object-cover rounded-md shadow-md" alt="Profile-pic"/>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section> {/* SKILLS LANGUAGES AND TECHNOLOGIES */}
+        <div className="bg-green-300 py-10 lg:px-64 xl:px-1/4">
+          <h2>Languages and Tech</h2>
+          <div className="flex bg-green-500">
+          <p>Vertical menus for the techs and languages</p>
+          <p>actual techs and languages</p>
+          </div>
         </div>
       </section>
       
@@ -282,6 +327,16 @@ function App() {
                 </div>
               </div>
             </div>
+          </div>
+        </div>
+      </section>
+
+      <section> {/* CONTACT ME - (WHAT'S NEXT?)*/}
+        <div className="bg-yellow-300 py-10 lg:px-64 xl:px-1/4">
+          <h2>Get In Touch</h2>
+          <div className="flex bg-yellow-500">
+          <p>Text for contacting me</p>
+          <button>Contact me</button>
           </div>
         </div>
       </section>
