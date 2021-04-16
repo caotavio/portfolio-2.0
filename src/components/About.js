6d7c1cce3759cc9119847e6422617bca8b874c6f
caotavio <otavio.araujodev@gmail.com>
2021-04-15 21:05:13 -0400

    General Info changes

--- src/components/About.js
+++ src/components/About.js
@@ -17,15 +17,15 @@ function About() {
           I changed career paths 3 years ago from a busy Dental Surgeon and Orthodontist in Brazil to an avid coder and programming enthusiast. I started learning code as a
           personal challenge. Well, this hobby became a passion (as in I couldn't do anything else with my free time other than code), so I decided to start a more structured
           course in Web Development with <span className="font-semibold text-lightsaberlight">JavaScript</span>, <span className="font-semibold text-lightsaberlight">Node.JS</span>,
-          <span className="font-semibold text-lightsaberlight">HTML</span> and <span className="font-semibold text-lightsaberlight">CSS</span>.
+           <span className="font-semibold text-lightsaberlight">HTML</span> and <span className="font-semibold text-lightsaberlight">CSS</span>.
           </p>
           <p>
           A year later, I decided to quit my job in Brazil and moved to Ireland to sharpen my English skills and also to be able to dedicate myself fully to coding and pursue
-          a career in it. I'm 31 now, and it has been a beautiful journey so far. Since I moved to Ireland, I reached C2 (proficient) level in English and secured a job as a
-          <span className="font-semibold text-lightsaberlight">Full Stack Web Developer</span> for a Digital Marketing agency.
+          a career in it. I'm 32 now, and it has been a beautiful journey so far. Since I moved to Ireland, I reached C2 (proficient) level in English and secured a job as a
+           <span className="font-semibold text-lightsaberlight">Full Stack Web Developer</span> for a Digital Marketing agency.
           </p>
           <p>
-          Now I can create web apps and websites using JavaScript and <span className="font-semibold text-lightsaberlight">TypeScript</span>, with <span className="font-semibold
+          Now, in this next chapter, I am a student at Cambrian College for Mobile Application Development in Toronto. Furthermore, with my previous experience, I can create web apps and websites using JavaScript and <span className="font-semibold text-lightsaberlight">TypeScript</span>, with <span className="font-semibold
           text-lightsaberlight">React</span> and <span className="font-semibold text-lightsaberlight">Angular</span> among other technologies listed below. This portfolio
           itself was built from scratch using <span className="font-semibold text-lightsaberlight">React and Tailwind</span>, which is a highly customizable utility-first
           CSS framework.
